2060b2c6ce432ba4ea08c699d4a432be6c81ad2e
Shpetim Shabanaj <118016169+Shpetim10@users.noreply.github.com>
2026-03-27 22:13:53 +0100

    Initial commit

---
+++ src/main/java/com/worknest/api/worknestapi/WorkNestApiApplication.java
@@ -0,0 +1,13 @@
+package com.worknest.api.worknestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkNestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkNestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/worknest/api/worknestapi/WorkNestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worknest.api.worknestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkNestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
